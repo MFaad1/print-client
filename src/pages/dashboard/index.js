05702c1b6760d51cd489d765fd14b7cd5dbb8f58
MFaad1 <faadsardar123@gmail.com>
2024-10-26 00:35:36 +0500

    fixes

--- src/pages/dashboard/index.js
+++ src/pages/dashboard/index.js
@@ -1,9 +1,63 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
 import { SideMenu, LineChart, ActivityChart } from "../../components";
 import Grid from "@mui/material/Grid";
 import "./index.css";
 import { Graph } from './../../svg'
+import { toast } from "react-toastify";
+import axios from "axios";
+import Loader from "../../components/Loader/Loader";
 const Dashboard = () => {
+    let agent_token = localStorage.getItem("Agent_access_token")
+  const [ordersList, setOrdersList] = useState();
+
+  const [loading, setloading] = useState(false);
+
+  const get_Customers = async () => {
+    try {
+
+      if (!agent_token) throw new Error("Please re-login and try again")
+      setloading(true)
+      let orders = await axios.get(`${process.env.REACT_APP_API_URL}/print-agent/summary`, {
+        headers: {
+          Authorization: `Bearer ${agent_token}`
+        }
+      });
+
+
+
+
+      setOrdersList(orders.data);
+
+
+
+    } catch (error) {
+      if (error.response && error.response.data && error.response.data.message) {
+        toast.error(error.response.data.message);
+        console.log(error.response.data.message)
+      }
+
+      else if (error.message) {
+        toast.error(error.message);
+      }
+      else {
+        toast.error("Internal server error");
+      }
+    }
+    finally {
+      setloading(false)
+
+    }
+  }
+
+
+
+
+
+  useEffect(() => {
+    get_Customers()
+}, [])
+
     return (
         <SideMenu>
             <div className="page-header">
@@ -39,44 +93,70 @@ const Dashboard = () => {
                     </div>
                 </Grid>
             </Grid>
+
+
+{loading ? <Loader/> : 
+
             <Grid container spacing={2} style={{ marginTop: "10px" }}>
                 <Grid item xs={12} sm={12} md={12} lg={7} xl={7}>
                     <Grid container spacing={2}>
                         <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
                             <div className="dashboard-box">
                                 <p className="dashboard-box-heading">Active Users</p>
-                                <p className="dashboard-box-value">287<span>/800</span></p>
+                                <p className="dashboard-box-value">
+                            {ordersList && ordersList.totalCustomers}
+                                    
+                                    </p>
                             </div>
                         </Grid>
                         <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
                             <div className="dashboard-box">
                                 <p className="dashboard-box-heading">Total Files</p>
-                                <p className="dashboard-box-value">3,298</p>
+                            
+                            <p className="dashboard-box-value">{ordersList && ordersList.totalPagesPrinted}</p>
                             </div>
                         </Grid>
                         <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
                             <div className="dashboard-box">
                                 <p className="dashboard-box-heading">Total Payment</p>
-                                <p className="dashboard-box-value">$10K</p>
+                                <p className="dashboard-box-value">$ {ordersList && ordersList.totalRevenue}</p>
                             </div>
                         </Grid>
+
                         <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
                             <div className="dashboard-box">
-                                <p className="dashboard-box-heading">Traffic</p>
-                                <p className="dashboard-box-value">64%</p>
+                                <p className="dashboard-box-heading">Orders</p>
+                                <p className="dashboard-box-value">{ordersList && ordersList.totalOrders}</p>
                             </div>
                         </Grid>
+
                         <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
                             <div className="dashboard-box">
-                                <p className="dashboard-box-heading">Current Month Sale</p>
-                                <p className="dashboard-box-value">86%</p>
+                                <p className="dashboard-box-heading">Total Complete Jobs</p>
+                                <p className="dashboard-box-value">{ordersList && ordersList.totalCompletedJobs}</p>
+                            </div>
+                        </Grid>
+
+                        <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
+                            <div className="dashboard-box">
+                                <p className="dashboard-box-heading">Total Pending Jobs</p>
+                                <p className="dashboard-box-value">{ordersList && ordersList.totalPendingJobs}</p>
+                            </div>
+                        </Grid>
+
+
+
+                        <Grid item xs={12} sm={4} md={4} lg={4} xl={4}>
+                            <div className="dashboard-box">
+                                <p className="dashboard-box-heading">Average Job Value</p>
+                                <p className="dashboard-box-value">{ordersList && ordersList.averageJobValue}</p>
                                 <img src={Graph} />
                             </div>
                         </Grid>
                         <Grid item xs={12} sm={4} md={1} lg={4} xl={4}>
                             <div className="dashboard-box">
-                                <p className="dashboard-box-heading">Current Year Sale</p>
-                                <p className="dashboard-box-value">+34%</p>
+                                <p className="dashboard-box-heading">Job Completion Rate</p>
+                                <p className="dashboard-box-value">{ordersList && ordersList.jobCompletionRate}</p>
                                 <img src={Graph} />
                             </div>
                         </Grid>
@@ -94,6 +174,9 @@ const Dashboard = () => {
                     </div>
                 </Grid>
             </Grid>
+}
+
+
         </SideMenu>
     );
 };
